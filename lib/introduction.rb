537e41ceb22c75e563962700f237735193e36221
Nicholas Baccete <nickbaccete@gmail.com>
2020-08-06 01:49:28 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -12,14 +12,14 @@ introduction "Dan"
 
 
 def introduction_with_language (name, language)
-  puts "hi, my name is #{name} and I am learning to program in #{language}."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 introduction_with_language (Dan, Ember.js)
 introduction_with_language (Josh, React)
 
 
 def introduction_with_language_optional (name, language = "Ruby")
-  puts "hi, my name is #{name} and I am learning to program in #{language}."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
 
